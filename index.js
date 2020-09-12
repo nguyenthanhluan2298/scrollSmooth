1a00322a9343a71a593a53323400b1541337d99c
unknown <luanluan2298@gmail.com>
2020-09-12 17:31:33 +0700

    ok oke

---
+++ index.js
@@ -0,0 +1,11 @@
+var smooth = document.querySelector('.to-top');
+console.log(smooth);
+window.addEventListener('scroll',function(){
+    if(window.pageYOffset > 200){
+        smooth.classList.add('active')
+    }
+    else{
+        smooth.classList.remove('active')
+    }
+
+}, 3000)
